c2267b35d23f9dae8c73ad75a4234e5b78685993
Mike Holzbach <maholzbach@gmail.com>
2019-04-27 13:02:06 -0400

    Still working on rendering on state update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,35 +6,40 @@ export const StoryContext = React.createContext(null);
 
 const App = () => {
   const [stories, setStories] = useState([]);
-  const [numberOfStories, setNumberOfStories] = useState(0);
+  const [storyIds, setStoryIds] = useState([]);
+  const [fetchComplete, setFetchComplete] = useState(false);
 
   useEffect(() => {
-    const newStoryArray = [];
-    const fetchData = async () => {
+    const fetchStoryIds = async () => {
       await axios
         .get("https://hacker-news.firebaseio.com/v0/topstories.json")
         .then(response => {
-          console.log(response.data.length);
-          setNumberOfStories(response.data.length);
-          console.log(numberOfStories);
-          response.data.forEach(story => {
-            axios
-              .get(`https://hacker-news.firebaseio.com/v0/item/${story}.json`)
-              .then(response => {
-                newStoryArray.push(response.data);
-              });
-          });
-          if (newStoryArray.length === numberOfStories) {
-            setStories(newStoryArray);
-          }
+          setStoryIds(response.data);
         });
     };
-    fetchData();
+    fetchStoryIds();
   }, []);
+
+  useEffect(() => {
+    const storiesToFetch = storyIds.slice(0, 20);
+    const fetchStories = async () => {
+      const newStoryArray = [];
+      await storiesToFetch.forEach(storyId => {
+        axios
+          .get(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+          .then(response => {
+            newStoryArray.push(response.data);
+          });
+      });
+      setStories(newStoryArray);
+      setFetchComplete(true);
+    };
+    fetchStories();
+  }, [storyIds]);
 
   return (
     <StoryContext.Provider value={stories}>
-      <div>{<StoryItem />}</div>
+      <div>{fetchComplete ? <StoryItem /> : <p>Loading...</p>}</div>
     </StoryContext.Provider>
   );
 };
--- src/components/StoryItem/StoryItem.jsx
+++ src/components/StoryItem/StoryItem.jsx
@@ -3,18 +3,23 @@ import { StoryContext } from "../App/App";
 
 const StoryItem = () => {
   const stories = useContext(StoryContext);
-  // const [localStoryState, setLocalStoryState] = useState([]);
-  // useEffect(() => {
-  //   setLocalStoryState(stories);
-  // }, [stories]);
+  const [localStoryState, setLocalStoryState] = useState([]);
   console.log("STORIES CONTEXT:", stories);
-  return (
-    <div>
-      {stories.map(story => {
-        return <h2 key={story.id}>{story.title}</h2>;
-      })}
-    </div>
-  );
+  const renderHeadlines = () => {
+    console.log("STORIES STATE:", localStoryState);
+    console.log("HERE");
+    localStoryState.map(story => {
+      console.log("STORY:", story);
+      return <h2 key={story.id}>{story.title}</h2>;
+    });
+  };
+  useEffect(() => {
+    setLocalStoryState(stories);
+  }, [stories]);
+  useEffect(() => {
+    renderHeadlines();
+  }, [localStoryState]);
+  return <div>{renderHeadlines()}</div>;
 };
 
 export default StoryItem;

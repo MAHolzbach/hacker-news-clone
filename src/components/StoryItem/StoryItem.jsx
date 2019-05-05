a549b309ff0c452a2161feb2edce5dd46d61478b
Mike Holzbach <maholzbach@gmail.com>
2019-05-05 17:44:24 -0400

    Fleshing out story component

--- src/components/StoryItem/StoryItem.jsx
+++ src/components/StoryItem/StoryItem.jsx
@@ -1,10 +1,20 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { StoryContext } from "../App/App";
 
 const StoryItem = () => {
   const context = useContext(StoryContext);
-  const stories = context.stories.map(story => <h3>{story.title}</h3>);
-  return <div className="story-div">{stories}</div>;
+  const storiesContent = context.stories.map(story => (
+    <>
+      <div className="stories__title-wrapper">
+        <strong className="stories__score">{story.score}</strong>
+        <a href={story.url}>
+          <h3>{story.title}</h3>
+        </a>
+      </div>
+      <p className="stories__poster">Posted by: {story.by}</p>
+    </>
+  ));
+  return <div className="story-div">{storiesContent}</div>;
 };
 
 export default StoryItem;
